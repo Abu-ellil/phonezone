4c66fabea5772e785af94b8d17fbe37e3da4223f
Abo Ellil <94858304+Abu-ellil@users.noreply.github.com>
2026-04-29 17:41:59 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
+    minimumCacheTTL: 60 * 60 * 24, // Cache optimized images for 24 hours
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   turbopack: {},
   reactStrictMode: true,
